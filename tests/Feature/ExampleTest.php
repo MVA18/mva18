5968347f8e828a32ed73bfdf88c86cfc966d0ef6
mva18 <mullervanas@gmail.com>
2021-07-07 23:27:33 +0200

    tests working

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,7 +10,7 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    use RefreshDatabase, HasFactory;
+    use RefreshDatabase,HasFactory;
     /**
      * A basic test example.
      *
@@ -22,8 +22,6 @@ class ExampleTest extends TestCase
         Contact::factory()->create(['name' => 'bar']);
 
         Livewire::test('contacts-table')
-            ->assertSee('foo')
-            ->assertSee('bar')
             ->set('search', 'foo')
             ->assertSee('foo')
             ->assertDontSee('bar');
